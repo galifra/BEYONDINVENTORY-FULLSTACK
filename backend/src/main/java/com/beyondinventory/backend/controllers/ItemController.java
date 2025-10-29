442aa98b9b0bcd2e51778f3e73bda3138bc7394b
galifra <gabrielukas@yahoo.com>
2025-10-29 12:29:28 -0500

    Update ItemController to link items with locationId and persist FK

--- backend/src/main/java/com/beyondinventory/backend/controllers/ItemController.java
+++ backend/src/main/java/com/beyondinventory/backend/controllers/ItemController.java
@@ -1,17 +1,29 @@
 package com.beyondinventory.backend.controllers;
 
 import com.beyondinventory.backend.models.Item;
+import com.beyondinventory.backend.models.Location;
 import com.beyondinventory.backend.repositories.ItemRepository;
+import com.beyondinventory.backend.repositories.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/items")
-@CrossOrigin(origins = "http://localhost:5173") // allows frontend to call API later
+@CrossOrigin(origins = "http://localhost:5173")
 public class ItemController {
 
     @Autowired
     private ItemRepository itemRepository;
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    // DTO (Data Transfer Object) for incoming request
+    public static class CreateItemRequest {
+        public String name;
+        public String description;
+        public Long locationId;
+    }
 
     // GET: Retrieve all items
     @GetMapping
@@ -19,9 +31,15 @@ public class ItemController {
         return itemRepository.findAll();
     }
 
-    // POST: Create a new item
+    // POST: Create a new item with locationId
     @PostMapping
-    public Item createItem(@RequestBody Item newItem) {
+    public Item createItem(@RequestBody CreateItemRequest request) {
+
+        Location location = locationRepository.findById(request.locationId)
+                .orElseThrow(() -> new RuntimeException("Location not found"));
+
+        Item newItem = new Item(request.name, location, request.description);
+
         return itemRepository.save(newItem);
     }
 }
